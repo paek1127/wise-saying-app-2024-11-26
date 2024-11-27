faad6cb6080a9fb05386e679303d19825340ff1b
paek1127 <paek1127@gmail.com>
2024-11-27 17:34:14 +0900

    1단계 TDD R

---
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -0,0 +1,27 @@
+package com.ll.domain.wiseSaying.controller;
+
+import com.ll.standard.util.TestUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WiseSayingControllerTest {
+    @Test
+    @DisplayName("== 명언 앱 ==")
+    public void t1() {
+        Scanner scanner = TestUtil.getScanner("종료");
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        App app = new App(scanner);
+        app.run();
+
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(output.toString())
+                .contains("== 명언 앱 ==");
+    }
+}
