d9ab5d10aa979041e07fd93f6ad0f049dee6831e
paek1127 <paek1127@gmail.com>
2024-11-27 18:01:25 +0900

    1단계 TDD B2

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,63 +1,7 @@
 package com.ll;
 
-import java.io.*;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        // lab1();
-        // lab2();
-        // lab3();
-        lab4();
-    }
 
-    // 사람이 직접 입력한 경우
-    private static void lab1() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("명령) ");
-        String cmd = scanner.nextLine().trim();
-
-        System.out.println("입력한 명령: " + cmd);
-    }
-
-    // 값을 미리 넣어놓고 출력하는 경우
-    private static void lab2() {
-        InputStream in = new ByteArrayInputStream("안녕\n잘가".getBytes());
-
-        Scanner scanner = new Scanner(in);
-
-        System.out.println("명령) ");
-        String cmd = scanner.nextLine().trim();
-
-        System.out.println("입력한 명령: " + cmd);
-
-        System.out.println("명령) ");
-        cmd = scanner.nextLine().trim();
-
-        System.out.println("입력한 명령: " + cmd);
-    }
-
-    public static void lab3() {
-        System.out.println("안녕하세요.");
-    }
-
-    public static void lab4() {
-        // 출력이 콘솔로 안나오게 설정
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(output));
-
-        System.out.println("안녕하세요.");
-
-        // 다시 콘솔로 나오게 설정
-        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
-
-        System.out.println("출력 : " + output.toString());
-
-        try {
-            output.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -1,12 +1,8 @@
 package com.ll.domain.wiseSaying.controller;
 
-import com.ll.App.App;
-import com.ll.standard.util.TestUtil;
+import com.ll.AppTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,33 +10,21 @@ public class WiseSayingControllerTest {
     @Test
     @DisplayName("== 명언 앱 ==")
     public void t1() {
-        Scanner scanner = TestUtil.getScanner("종료");
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+        String output = AppTest.run("종료");
 
-        App app = new App(scanner);
-        app.run();
-
-        TestUtil.clearSetOutToByteArray(output);
-
-        assertThat(output.toString())
+        assertThat(output)
                 .contains("== 명언 앱 ==");
     }
 
     @Test
     @DisplayName("명령) ")
     public void t2() {
-        Scanner scanner = TestUtil.getScanner("""
+        String output = AppTest.run("""
                 목록
                 종료
                 """);
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
 
-        App app = new App(scanner);
-        app.run();
-
-        TestUtil.clearSetOutToByteArray(output);
-
-        assertThat(output.toString())
+        assertThat(output)
                 .contains("명령) ");
     }
 }
