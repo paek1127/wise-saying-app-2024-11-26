40a725e13b7f8f9358bab14031347c6f402b54fa
paek1127 <paek1127@gmail.com>
2024-11-27 19:54:18 +0900

    1단계 리팩토링 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,17 +1,29 @@
 package com.ll;
 
+import com.ll.domain.system.controller.SystemController.SystemController;
+
 import java.util.Scanner;
 
 public class App {
     private final Scanner scanner;
+    private final SystemController systemController;
 
     public App(Scanner scanner) {
         this.scanner = scanner;
+        this.systemController = new SystemController();
     }
 
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-        System.out.println("명령) ");
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine();
+
+            if (cmd.equals("종료")) {
+                systemController.actionExit();
+                break;
+            }
+        }
     }
 }
--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -10,7 +10,7 @@ public class WiseSayingControllerTest {
     @Test
     @DisplayName("== 명언 앱 ==")
     public void t1() {
-        String output = AppTest.run("종료");
+        String output = AppTest.run("");
 
         assertThat(output)
                 .contains("== 명언 앱 ==");
@@ -21,10 +21,21 @@ public class WiseSayingControllerTest {
     public void t2() {
         String output = AppTest.run("""
                 목록
-                종료
                 """);
 
         assertThat(output)
                 .contains("명령) ");
+    }
+
+    @Test
+    @DisplayName("명령을 2번 이상 입력할 수 있습니다.")
+    public void t3() {
+        String output = AppTest.run("""
+                목록
+                목록
+                """);
+
+        String[] split = output.split("명령\\)");
+        assertThat(split).hasSize(4);
     }
 }
