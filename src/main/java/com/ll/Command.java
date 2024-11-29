aa611cd4187ae97c957e214a886dc77aab9db122
paek1127 <paek1127@gmail.com>
2024-11-29 11:37:12 +0900

    command f5 g

--- src/main/java/com/ll/Command.java
+++ src/main/java/com/ll/Command.java
@@ -16,7 +16,7 @@ public class Command {
         String[] params = queryString.split("&");
 
         for (String param : params) {
-            String[] paramBits = param.split("=",2);
+            String[] paramBits = param.split("=", 2);
             this.params.put(paramBits[0], paramBits[1]);
         }
     }
@@ -31,6 +31,20 @@ public class Command {
 
     // 오버로딩
     public String getParam(String key, String defaultValue) {
-        return params.getOrDefault(key,defaultValue);
+        return params.getOrDefault(key, defaultValue);
+    }
+
+    public int getParamAsInt(String key, int defaultValue) {
+        String value = getParam(key);
+
+        if (value == null) {
+            return defaultValue;
+        }
+
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
     }
 }
