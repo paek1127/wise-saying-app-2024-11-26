0c77b78d48e38b9f539c1ddf39c4f62fed282e4b
paek1127 <paek1127@gmail.com>
2024-12-06 12:59:42 +0900

    findById b

--- src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
+++ src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,21 +30,11 @@ public class WiseSayingFileRepositoryTest {
         WiseSaying wiseSaying = new WiseSaying(0, "명언1", "저자1");
         wiseSayingRepository.save(wiseSaying);
 
-        String filePath = WiseSayingFileRepository.getRowFilePath(wiseSaying.getId());
+        Optional<WiseSaying> opWiseSaying = wiseSayingRepository.findById(wiseSaying.getId());
 
         assertThat(
-                Util.file.exists(filePath)
-        ).isTrue();
-
-        String jsonStr = Util.file.get(filePath, "");
-
-        Map<String, Object> wiseSayingMap = Util.json.toMap(jsonStr);
-        WiseSaying wiseSayingRestored = new WiseSaying(wiseSayingMap);
-
-        System.out.println(wiseSayingRestored);
-        System.out.println(wiseSaying);
-
-        assertThat(wiseSayingRestored).isEqualTo(wiseSaying);
+                opWiseSaying.get()
+        ).isEqualTo(wiseSaying);
     }
 
     @Test
