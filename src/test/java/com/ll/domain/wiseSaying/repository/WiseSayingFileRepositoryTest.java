b587965a53265da0d50b3b502fc96bdac5a4ee7d
paek1127 <paek1127@gmail.com>
2024-12-06 12:54:49 +0900

    findById r/g

--- src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
+++ src/test/java/com/ll/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
@@ -2,9 +2,13 @@ package com.ll.domain.wiseSaying.repository;
 
 import com.ll.domain.wiseSaying.entity.WiseSaying;
 import com.ll.standard.util.Util;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.Map;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -57,5 +61,18 @@ public class WiseSayingFileRepositoryTest {
         assertThat(
                 Util.file.exists(filePath)
         ).isFalse();
+    }
+
+    @Test
+    @DisplayName("명언 단건 조회")
+    public void t3() {
+        WiseSaying wiseSaying = new WiseSaying(0, "명언1", "저자1");
+        wiseSayingRepository.save(wiseSaying);
+
+        Optional<WiseSaying> opWiseSaying = wiseSayingRepository.findById(wiseSaying.getId());
+
+        assertThat(
+                opWiseSaying.get()
+        ).isEqualTo(wiseSaying);
     }
 }
