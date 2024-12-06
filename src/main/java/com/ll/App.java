8d77479c52579f26bbe6b9d6c785c9ba640453f2
paek1127 <paek1127@gmail.com>
2024-12-06 11:55:55 +0900

    파일경로수정

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,6 +1,6 @@
 package com.ll;
 
-import com.ll.domain.system.controller.SystemController.SystemController;
+import com.ll.domain.system.controller.SystemController;
 import com.ll.domain.wiseSaying.controller.WiseSayingController;
 
 import java.util.Scanner;
